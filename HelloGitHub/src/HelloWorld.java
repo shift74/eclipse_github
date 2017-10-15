5abdf651f9f7d4f55d0b228945e87fdcaeb3ebe4
shift74 <shift74@shift74-VirtualBox>
2017-10-15 18:29:26 +0900

    The additional hotfix change

--- HelloGitHub/src/HelloWorld.java
+++ HelloGitHub/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello world");
 		System.out.println("Hello GitHub Again");
 		System.out.println("Hello What is this Again");
+		System.out.println("Hello XXXX this Again");
 
 	}
 

9a168ba9ca4e427fdd49745565d5057e04e31fc8
shift74 <30859098+shift74@users.noreply.github.com>
2017-10-15 20:57:23 +0900

    github for git pull

--- HelloGitHub/src/PushPrint.java
+++ HelloGitHub/src/PushPrint.java
@@ -6,6 +6,8 @@ public class PushPrint {
 		System.out.println("Hello PushPrint");
 		System.out.println("Hello YYYYYYY-------PushPrint");
 		System.out.println("Hello XXXXX--PushPrint");
+		
+		System.out.println("Hello ZZZZZZ--PushPrint");
 	}
 
 }
